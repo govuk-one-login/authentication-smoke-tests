83c75b39f0b286bb891f234f4f0caa6dd0479874
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-10-20 16:11:07 +0100

    GOVSI-619: update pagerduty alert descriptions and change thresholds

--- ci/terraform/alarm.tf
+++ ci/terraform/alarm.tf
@@ -5,32 +5,32 @@ resource "aws_cloudwatch_metric_alarm" "smoke_tester_metric_alarm_p1" {
   evaluation_periods  = "1"
   metric_name         = "Failed"
   namespace           = "CloudWatchSynthetics"
-  period              = "300"
+  period              = "600"
   statistic           = "Sum"
-  threshold           = "2"
+  threshold           = "3"
 
   dimensions = {
     CanaryName = aws_synthetics_canary.smoke_tester.name
   }
 
-  alarm_description = "Monitor smoke-tester P1 failures"
+  alarm_description = "GOV.UK Sign in - ${local.smoke_tester_name} P1 failure"
   alarm_actions     = [aws_sns_topic.pagerduty_p1_alerts.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "smoke_tester_metric_alarm_p2" {
   alarm_name          =  "${local.smoke_tester_name}-metric-alarm_p2"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = "3"
   metric_name         = "Failed"
   namespace           = "CloudWatchSynthetics"
-  period              = "300"
+  period              = "600"
   statistic           = "Sum"
-  threshold           = "2"
+  threshold           = "5"
 
   dimensions = {
     CanaryName = aws_synthetics_canary.smoke_tester.name
   }
 
-  alarm_description = "Monitor smoke-tester P2 failures"
+  alarm_description = "GOV.UK Sign in - ${local.smoke_tester_name} P2 failure"
   alarm_actions     = [aws_sns_topic.pagerduty_p2_alerts.arn]
 }
