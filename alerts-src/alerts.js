4444f2f557d144fb3905012d7b8c39940a899f67
james.howes <james.howes@digital.cabinet-office.gov.uk>
2021-10-29 17:48:04 +0100

    BAU - Improve formatting of Slack alerts
    - Use slack attachments.
    - Check the NewStateValue and make the slack message green if OK and red if ALARM.

--- alerts-src/alerts.js
+++ alerts-src/alerts.js
@@ -6,9 +6,13 @@ const { getParameter } = require("./aws");
 exports.handler = async function(event, context) {
     console.log("Alert lambda triggered");
     const slackHookUrl = await getParameter(process.env.DEPLOY_ENVIRONMENT+"-slack-hook-url");
-
-    var data = JSON.stringify({
-            "text":event.Records[0].Sns.Message});
+    var snsMessage = JSON.parse(event.Records[0].Sns.Message);
+    var colorCode = "#C70039";
+    if(snsMessage.NewStateValue === "OK") {
+        colorCode = "#36a64f"
+    }
+     var data = JSON.stringify({
+      "attachments": [{"fallback": snsMessage.AlarmDescription,"color": colorCode,"title": snsMessage.AlarmName,"text": "Alarm Description: " + snsMessage.AlarmDescription,"fields": [{"title": "Status","value": snsMessage.NewStateValue,"short": false}],"footer": "GOV.UK Sign In alert"}]});
     var config = {
       method: 'post',
       url: slackHookUrl,
