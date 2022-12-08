4edf604e8c512cf42a8f542137b76a2b254fca19
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2022-12-08 16:21:19 +0000

    AUT-829: Update descriptions for new and existing smoke tests
    Description was incorrect for the existing sign up smoke test.

--- ci/terraform/alarm.tf
+++ ci/terraform/alarm.tf
@@ -14,7 +14,7 @@ resource "aws_cloudwatch_metric_alarm" "smoke_tester_metric_alarm_p1" {
     CanaryName = aws_synthetics_canary.smoke_tester.name
   }
 
-  alarm_description = "GOV.UK Sign in - ${local.smoke_tester_name} (create account smoke test) P1 alarm"
+  alarm_description = "GOV.UK Sign in - ${local.smoke_tester_name} (sign in smoke test) P1 alarm"
   alarm_actions     = [var.environment == "production" ? aws_sns_topic.pagerduty_p1_alerts.arn : data.aws_sns_topic.slack_events.arn]
   ok_actions        = [var.environment == "production" ? aws_sns_topic.pagerduty_p1_alerts.arn : data.aws_sns_topic.slack_events.arn]
 }
@@ -34,7 +34,7 @@ resource "aws_cloudwatch_metric_alarm" "smoke_tester_metric_alarm_p2" {
     CanaryName = aws_synthetics_canary.smoke_tester.name
   }
 
-  alarm_description = "GOV.UK Sign in - ${local.smoke_tester_name} (create account smoke test) P2 alarm"
+  alarm_description = "GOV.UK Sign in - ${local.smoke_tester_name} (sign in smoke test) P2 alarm"
   alarm_actions     = [var.environment == "production" ? aws_sns_topic.pagerduty_p2_alerts.arn : data.aws_sns_topic.slack_events.arn]
   ok_actions        = [var.environment == "production" ? aws_sns_topic.pagerduty_p2_alerts.arn : data.aws_sns_topic.slack_events.arn]
 }
