6e7b15f940ff065c6831e6d7cc8ebaec571f7cb7
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2023-01-20 16:54:14 +0000

    AUT-834: Put microlient in separate module

---
+++ src/client.js
@@ -0,0 +1,33 @@
+const log = require("SyntheticsLogger");
+const express = require("express");
+const app = express();
+const { auth } = require("express-openid-connect");
+const crypto = require("crypto");
+
+const startClient = async (port, scope, clientId, clientBaseUrl, issuerBaseURL, clientPrivateKey) => {
+  
+  log.info("Starting Client");
+  app.use(
+    auth({
+      issuerBaseURL: issuerBaseURL,
+      baseURL: clientBaseUrl,
+      clientID: clientId,
+      secret: crypto.randomBytes(20).toString("base64url"),
+      clientAuthMethod: "private_key_jwt",
+      clientAssertionSigningKey: clientPrivateKey,
+      idTokenSigningAlg: "ES256",
+      authRequired: true,
+      authorizationParams: {
+        response_type: "code",
+        scope: scope,
+      },
+    })
+  );
+  app.get("/", async (req, res) => {
+    const userinfo = await req.oidc.fetchUserInfo();
+    res.json(userinfo);
+  });
+  return app.listen(port);
+};
+
+module.exports = { startClient };
