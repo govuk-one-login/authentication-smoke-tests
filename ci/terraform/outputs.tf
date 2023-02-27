71d193b2b6328df8ff7f8025ddead07a573a431e
james.howes <james.howes@digital.cabinet-office.gov.uk>
2023-02-27 13:00:37 +0000

    AUT-1087 - Remove old sign in Canary
    - We have a new Canary which executes the sign in flow and uses the terraform module. The old one is redundant and can be removed

--- ci/terraform/outputs.tf
+++ ci/terraform/outputs.tf
@@ -1,11 +1,3 @@
-output "canary_id" {
-  value = aws_synthetics_canary.smoke_tester.id
-}
-
-output "canary_arn" {
-  value = aws_synthetics_canary.smoke_tester.arn
-}
-
 output "smoke_tests_client_id" {
   value = random_string.stub_rp_client_id[0].result
 }
