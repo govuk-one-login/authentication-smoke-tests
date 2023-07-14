b6a3dddf89c8592d9ed2e9339c2c31624e84f228
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2023-07-14 11:22:14 +0100

    Modify build.yml to build all dependent modules

---
+++ heartbeat/heartbeat.js
@@ -0,0 +1,12 @@
+const cronitor = require('cronitor')(process.env.CRONITOR_API_KEY);
+
+const handler = async function (event, context) { // eslint-disable-line no-unused-vars
+    const monitorKey = process.env.CRONITOR_MONITOR_KEY;
+    console.log("cronitor ping monitor " + monitorKey);
+    const monitor = new cronitor.Monitor(monitorKey);
+    return monitor.ping().then((response) => {
+        return "cronitor ping monitor " + monitorKey + " returns: " + response;
+    });
+}
+
+module.exports = { handler }
---
+++ slack/aws.js
@@ -0,0 +1,17 @@
+const {
+  SSM
+} = require("@aws-sdk/client-ssm");
+
+const client = new SSM();
+
+const getParameter = async (parameterName) => {
+
+  const result = await client.getParameter({
+    Name: `${parameterName}`,
+    WithDecryption: true,
+  });
+
+  return result.Parameter.Value;
+};
+
+module.exports = { getParameter };
