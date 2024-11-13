4bf7990b9f9bf37ec87076554bdd09d537c96aca
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2024-11-13 12:54:51 +0000

    BAU: Upgrade ESLint system for v9
    A new major version of ESLint changes how it should be configured. A new `eslint.config.js` file replaces `.eslintrc.json` with tweaks to how configuration is done.
    
    `eslint-plugin-n` is also brought in as a recommended fork of `eslint-plugin-node` that is no longer maintained. The new plugin adds a new rule that warns against use of `fetch` as it's not supported until Node 21. As it's already in place an exception is added to ignore the rule for `fetch`.
    
    Further, unused disable directives in `alert.js` and `slack.js` are removed.

--- slack/slack.js
+++ slack/slack.js
@@ -16,7 +16,6 @@ const handler = async function (event, context) {
   };
   console.log("Sending alert to slack");
   try {
-    // eslint-disable-next-line no-undef
     const response = await fetch(slackHookUrl, config);
     const message = await response.text();
     console.log(message);
