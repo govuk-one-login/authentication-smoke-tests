03921b4da7cd7db0ea380b0e84938e0d573d66da
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2024-12-11 12:08:20 +0000

    BAU: remove vestigial flags from deploy script

--- scripts/dev_deploy_common.sh
+++ scripts/dev_deploy_common.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-[[ "${BASH_SOURCE[0]}" != "${0}" ]] || {
-    echo "Error: Script must be sourced, not executed"
-    exit 1
+[[ ${BASH_SOURCE[0]} != "${0}" ]] || {
+  echo "Error: Script must be sourced, not executed"
+  exit 1
 }
 
 function runTerraform() {
-    echo "Running ${1} Terraform..."
-    pushd "${DIR}/ci/terraform/${1}" >/dev/null
-    rm -rf .terraform/
-    terraform init -backend-config=sandpit.hcl
-    terraform apply -var-file sandpit.tfvars -var-file sandpit-stub-clients.tfvars ${2}
-    popd >/dev/null
+  echo "Running ${1} Terraform..."
+  pushd "${DIR}/ci/terraform/${1}" > /dev/null
+  rm -rf .terraform/
+  terraform init -backend-config=sandpit.hcl
+  terraform apply -var-file sandpit.tfvars -var-file sandpit-stub-clients.tfvars "${2}"
+  popd > /dev/null
 }
 
 function usage() {
-    cat <<USAGE
+  cat << USAGE
   A script to deploy the GOV.UK Sign in smoke tests to the sandpit environment.
   Requires a GDS CLI, AWS CLI and jq installed and configured.
 
   Usage:
-    $0 [-b|--build] [-c|--clean] [-s|--shared] [-o|--oidc] [-a|--account-management] [--audit] [--destroy] [-p|--prompt]
+    $0 [-b|--build] [-p|--prompt] [--destroy]
 
   Options:
     -b, --build               run yarn install and build tasks (default)
@@ -35,35 +35,35 @@ USAGE
 BUILD=0
 TERRAFORM_OPTS="-auto-approve"
 if [[ $# == 0 ]] || [[ $* == "-p" ]]; then
-    BUILD=1
+  BUILD=1
 fi
 while [[ $# -gt 0 ]]; do
-    case $1 in
+  case $1 in
     -b | --build)
-        BUILD=1
-        ;;
+      BUILD=1
+      ;;
     --destroy)
-        TERRAFORM_OPTS="-destroy"
-        ;;
+      TERRAFORM_OPTS="-destroy"
+      ;;
     -p | --prompt)
-        TERRAFORM_OPTS=""
-        ;;
+      TERRAFORM_OPTS=""
+      ;;
     *)
-        usage
-        exit 1
-        ;;
-    esac
-    shift
+      usage
+      exit 1
+      ;;
+  esac
+  shift
 done
 
-if [[ $BUILD == "1" ]]; then
-    echo "Building deployment artefacts ... "
-    pushd "${DIR}" >/dev/null
-    yarn clean && yarn install --production && yarn build
-    mkdir -p ../release-artefacts/
-    cp dist/*.zip ../release-artefacts/
-    popd >/dev/null
-    echo "done!"
+if [[ ${BUILD} == "1" ]]; then
+  echo "Building deployment artefacts ... "
+  pushd "${DIR}" > /dev/null
+  yarn clean && yarn install --production && yarn build
+  mkdir -p ../release-artefacts/
+  cp dist/*.zip ../release-artefacts/
+  popd > /dev/null
+  echo "done!"
 fi
 
 # shellcheck source=./scripts/export_aws_creds.sh
