03ee7d0efe229b30fa3df5afb566c81aceea7995
Matt Votsikas McLean <matt.votsikas-mclean@digital.cabinet-office.gov.uk>
2025-08-13 08:46:34 +0100

    BAU: Hide most characters of Cronitor key
    Instead of logging the full key, hide all but the last 3. This means less risk of exposure.

--- heartbeat/heartbeat.js
+++ heartbeat/heartbeat.js
@@ -3,10 +3,13 @@ const cronitor = require("cronitor")(process.env.CRONITOR_API_KEY);
 // eslint-disable-next-line no-unused-vars
 const handler = async function (event, context) {
   const monitorKey = process.env.CRONITOR_MONITOR_KEY;
-  console.log("cronitor ping monitor " + monitorKey);
+  const keyExcerpt = monitorKey.slice(-2);
+  console.log("cronitor ping monitor ending " + keyExcerpt);
   const monitor = new cronitor.Monitor(monitorKey);
   return monitor.ping().then((response) => {
-    return "cronitor ping monitor " + monitorKey + " returns: " + response;
+    return (
+      "cronitor ping monitor ending " + keyExcerpt + " returns: " + response
+    );
   });
 };
 
