218d5df9b2dcb9cc8f45c9c6f78566bdb4c9f128
Pawankumar Kushwaha <pawankumar.kushwaha@digital.cabinet-office.gov.uk>
2025-09-18 14:17:43 +0100

    AUT-4732: deploy smoke test in AUthdev3

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ Contains source code which is run by AWS Lambdas. The live status of smoke tests
 Changes to smoke tests can be tested by deploying to the Dev environment.
 Running `./scripts/sam-deploy-dev.sh` will trigger a GHA workflow on current checkout branch ( build-and-deploy-dev-sp.yml) and the GHA workflow trigger's codepipeline dev-smoke-test-pipeline
 
+## Deploying to authdev3 ( Orch integrated dev env)
+
+Changes to smoke tests can be tested by deploying to the Authdev3 environment.
+Running `./sam-deploy-authdev3.sh` will trigger a local SAM deploy on current checkout branch , it will update the cloudformation stack authdev3-smoke-test-canary in eu-west-2
+
 ## Formatting
 
 Run formatting on the javascript files:
---
+++ sam-deploy-authdevs.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Ensure we're in the root directory of the repo
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+
+if [ $# -ne 0 ]; then
+  echo "Error: This script does not accept any arguments"
+  exit 1
+fi
+
+# -------------
+# Prerequisites
+# -------------
+if ! command -v sam &> /dev/null; then
+  echo "Deploying template requires AWS sam cli to be installed. See https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-sam-cli.html for installation instructions."
+  exit 1
+fi
+
+function sso_login() {
+  export AWS_ACCOUNT=di-authentication-development
+  export AWS_PROFILE=di-authentication-development-AWSAdministratorAccess
+  export AWS_REGION="eu-west-2"
+
+  if ! aws sts get-caller-identity &> /dev/null; then
+    aws sso login --profile "${AWS_PROFILE}" || exit 1
+  fi
+}
+
+SAMCONFIG_FILE=${SAMCONFIG_FILE:-${DIR}/scripts/authdev3-samconfig.toml}
+
+cd "${DIR}"
+
+printf "Deploying in environment %s\n" "authdev3"
+read -r -p "Review changeset before deployment? (y/n): " confirm_choice
+if [[ ${confirm_choice} =~ ^[Yy]$ ]]; then
+  CONFIRM_CHANGESET_OPTION="--confirm-changeset"
+else
+  CONFIRM_CHANGESET_OPTION="--no-confirm-changeset"
+fi
+
+echo "Building deployment artefacts ... "
+yarn run build:all
+echo "done!"
+
+sso_login
+
+aws s3 cp "${DIR}/dist/synthetics.zip" s3://authdev3-smoke-test-githubartifactsourcebucket/synthetics.zip
+
+echo "Lint template file"
+sam validate --lint
+
+echo "Running sam build on template file"
+sam build --parallel
+
+sam deploy \
+  --no-fail-on-empty-changeset \
+  --config-env "authdev3" \
+  --config-file "${SAMCONFIG_FILE}" \
+  ${CONFIRM_CHANGESET_OPTION}
+
+echo "Deployment complete!"
